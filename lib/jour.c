afb8ed4ac545d26bf9cae1f1c29b5e013817d9d5
Jojokes <83455892+jojo8356@users.noreply.github.com>
2025-10-21 14:27:41 +0200

    Update jour.c

--- lib/jour.c
+++ lib/jour.c
@@ -14,6 +14,9 @@
 
 // char *
 // https://but.lyceum.fr/r101-init-dev/c07/#recherche-avec-strstr
+// structure fonction
+// https://but.lyceum.fr/r101-init-dev/c01/#d%C3%A9finition-dune-fonction
+// fonction char *
 char *get_CA_day()
 {
     float pu = 0;
